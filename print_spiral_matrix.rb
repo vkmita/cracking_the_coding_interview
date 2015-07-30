c2d100a7f4c9e9b1c1ebef02ba25390af5c811d5
vkmita <me@vkmita.com>
2015-07-30 00:07:28 -0700

    refactor a bit

--- print_spiral_matrix.rb
+++ print_spiral_matrix.rb
@@ -12,9 +12,6 @@ class DiagonalMatrixPrinter
     self.width = matrix.first.length - 1
     self.height = matrix.length - 2
 
-    self.current_direction_x = 'right'
-    self.current_direction_y = 'down'
-
     self.current_x = 0
     self.current_y = 0
 
@@ -25,46 +22,43 @@ class DiagonalMatrixPrinter
     [:right, :down, :left, :up].cycle do |direction|
       case direction
       when :right
-        right_left((current_x..(current_x + width)).to_a, 1)
+        right_left((current_x.upto(current_x + width)), 1)
       when :down
-        down_up((current_y..(current_y + height)).to_a, -1)
+        down_up((current_y.upto(current_y + height)), -1)
       when :left
-        right_left(((current_x - width)..current_x).to_a.reverse, -1)
+        right_left(current_x.downto(current_x - width), -1)
       when :up
-        down_up(((current_y - height)..current_y).to_a.reverse, 1)
+        down_up(current_y.downto(current_y - height), 1)
       end
 
-      if width < 0 && height < 0
+      if width < 0 || height < -1
         return puts "#{result}"
       end
     end
   end
 
-  def right_left(array_range, y_change)
-    array_range.each do |temp_x|
-      puts "#{temp_x} #{current_y} : #{matrix[current_y][temp_x]}"
-      self.current_x = temp_x
-      self.result <<  matrix[current_y][temp_x]
+  def right_left(range, y_change)
+    range.each do |current_x|
+      puts "#{current_x} #{current_y} : #{matrix[current_y][current_x]}"
+      self.current_x = current_x
+      self.result <<  matrix[current_y][current_x]
     end
 
     self.width -= 1
     self.current_y = current_y + y_change
   end
 
-  def down_up(array_range, x_change)
-    array_range.each do |temp_y|
-      puts "#{current_x} #{temp_y} : #{matrix[temp_y][current_x]}"
-      self.current_y = temp_y
-      result <<  matrix[temp_y][current_x]
+  def down_up(range, x_change)
+    range.each do |current_y|
+      puts "#{current_x} #{current_y} : #{matrix[current_y][current_x]}"
+      self.current_y = current_y
+      result <<  matrix[current_y][current_x]
     end
 
     self.height -= 1
     self.current_x = current_x + x_change
   end
 end
-
-
-
 
 matrix = [
   [1, 2, 3],
@@ -75,10 +69,26 @@ matrix = [
 DiagonalMatrixPrinter.new(matrix).print
 
 matrix = [
-  [1,  2, 3, 4],
+  [1,   2,  3, 4],
   [12, 13, 14, 5],
   [11, 16, 15, 6],
-  [10, 9, 8, 7]
+  [10,  9,  8, 7]
+]
+
+DiagonalMatrixPrinter.new(matrix).print
+
+matrix = [
+  [1,   2,  3, 4, 5, 6],
+  [12, 11, 10, 9, 8, 7]
+]
+
+DiagonalMatrixPrinter.new(matrix).print
+
+matrix = [
+  [1, 2],
+  [3, 3],
+  [7, 4],
+  [6, 5]
 ]
 
 DiagonalMatrixPrinter.new(matrix).print
